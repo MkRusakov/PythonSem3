c03c6420d7640a92e24af74e384e0cd98b60362a
Mikhail <i@MkRusakov.ru>
2022-10-02 13:30:04 +0300

    Task2

--- Task2.py
+++ Task2.py
@@ -1,8 +1,15 @@
 # Напишите программу, которая найдёт произведение пар чисел списка. Парой считаем первый и последний элемент, второй и предпоследний и т.д.
-list = [1, 5, 7, 2, 0, 3]
+list = [1, 5, 7, 2, 0, 3, 4]
 count = int(len(list)/2)
 current = int(len(list)-1)
-for i in range(0, count, +1):
-    result = list[i] * list[current]
-    current -= 1
-    print(result)
+if int(len(list)) % 2 == 0:
+    for i in range(0, count, +1):
+        result = list[i] * list[current]
+        current -= 1
+        print(result)
+else:
+    for i in range(0, count, +1):
+        result = list[i] * list[current]
+        current -= 1
+        print(result)
+    print(list[count] * list[count])
